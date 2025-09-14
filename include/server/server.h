6189e99f30478258c11f6c9c697f5cf2313ad1b5
banana584 <alexander.david.watts@gmail.com>
2025-09-14 13:10:42 +0100

    Update server.h
    Updated server to use linked list instead of array to store clients - I am planning to use a basic block allocator of sockets - and added declaration of Server_Disconnect to disconnect a socket.

--- include/server/server.h
+++ include/server/server.h
@@ -1,10 +1,16 @@
 #include "../sockets/sockets.h"
 
+typedef struct Node {
+  struct Node* next;
+  int index;
+  Socket* data;
+} Node;
+
 typedef struct Server {
   Socket* server_sock;
   struct sockaddr_in addr;
   socklen_t addr_len;
-  Socket** client_socks;
+  Node* client_socks_head;
   size_t num_clients;
   size_t max_clients;
 } Server;
@@ -14,3 +20,5 @@ Server* Server_Create(int port);
 void Server_Destroy(Server* server);
 
 void Server_Accept(Server* server);
+
+void Server_Disconnect(Server* server, int idx);
