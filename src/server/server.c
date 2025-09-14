abd66011b44cf80bf8b4848e65d80f9c6ce798b4
banana584 <alexander.david.watts@gmail.com>
2025-09-14 10:45:45 +0100

    Update server.c
    Added definitions for Server_Create and Server_Destroy, need to add 2 more fields to Server struct, add more functions and add error checking.

--- src/server/server.c
+++ src/server/server.c
@@ -1,1 +1,34 @@
 #include "../../include/server/server.h"
+
+Server* Server_Create(int port) {
+  Server* server = (Server*)malloc(sizeof(Server));
+
+  server->server_sock = Socket_Create();
+
+  server->addr.sin_family = AF_INET;
+  server->addr.sin_port = htons(port);
+  server->addr.sin_addr.s_addr = INADDR_ANY;
+  server->addr_len = sizeof(server->addr);
+
+  server->client_socks = NULL;
+  server->num_clients = 0;
+  server->max_clients = 0;
+
+  return server;
+}
+
+void Server_Destroy(Server* server) {
+  if (!server) {
+    return;
+  }
+
+  if (server->client_socks) {
+    for (int i = 0; i < server->num_clients; i++) {
+      Socket_Destroy(server->client_socks[i]);
+    }
+    free(server->client_socks);
+  }
+
+  Socket_Destroy(server->server_sock);
+
+  free(server);
