34384f967864fdf2e00c7a7aa0903342df5ba220
banana584 <alexander.david.watts@gmail.com>
2025-09-14 10:35:53 +0100

    Update server.h
    Added declarations of server struct and functions to create and destroy instances of server struct. Implementations of function still need to be created.

--- include/server/server.h
+++ include/server/server.h
@@ -1,1 +1,12 @@
+#include "../sockets/sockets.h"
 
+typedef struct Server {
+  Socket* server_sock;
+  struct sockaddr_in addr;
+  socklen_t addr_len;
+  Socket** client_socks;
+} Server;
+
+Server* Server_Create(int port);
+
+void Server_Destroy(Server* server);
